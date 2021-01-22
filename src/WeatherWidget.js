4bf2fcccab47ec968d3d1e300c055b1ec0776a70
NikitOs <exemplum777@gmail.com>
2021-01-22 18:36:51 +0300

    Init project

---
+++ src/WeatherWidget.js
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from 'react'
+import './WeatherWidget.css'
+
+const WeatherWidget = () => {
+
+  const [temperature, setTemperature] = useState(0);
+  const [isSnow, setSnow] = useState(false);
+  const [isRain, setRain] = useState(false);
+  const [isStrongWind, setStrongWind] = useState(false);
+
+  useEffect(() => {
+    console.log('Произошли изменения');
+  }, [temperature,
+    isSnow,
+    isRain,
+    isStrongWind,
+  ]);
+
+  return (
+    <div className="weather-widget">
+      <div className="weather-search">
+        <input type="text" className="weather-serch_temperature" placeholder="Введите текующую температуру" value={temperature} onChange={event => setTemperature(event.target.value)} />
+        <label>
+          Идёт снег:
+          <input type="checkbox" checked={isSnow} onChange={event => setSnow(event.target.checked)} />
+        </label>
+        <label>
+          Идёт Дождь:
+          <input type="checkbox" checked={isRain} onChange={event => setRain(event.target.checked)} />
+        </label>
+        <label>
+          Сильный ветер:
+          <input type="checkbox" checked={isStrongWind} onChange={event => setStrongWind(event.target.checked)} />
+        </label>
+      </div>
+      
+      Weather widget
+
+    </div>
+  )
+}
+
+export default WeatherWidget;
