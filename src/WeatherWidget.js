34adc40c2d4cb3e68cb8b6fb73bc52ab765bf0e5
NikitOs <exemplum777@gmail.com>
2021-01-26 13:14:55 +0300

    Implements weather controller

--- src/WeatherWidget.js
+++ src/WeatherWidget.js
@@ -2,40 +2,58 @@ import React, {useEffect, useState} from 'react'
 import './WeatherWidget.css'
 
 const WeatherWidget = () => {
-
   const [temperature, setTemperature] = useState(0);
-  const [isSnow, setSnow] = useState(false);
+  const [isSun, setSun] = useState(true);
+  const [isCloud, setCloud] = useState(false);
   const [isRain, setRain] = useState(false);
-  const [isStrongWind, setStrongWind] = useState(false);
+  const [isStrongRain, setStrongRain] = useState(false);
+  const [iconClasses, setClasses] = useState(['weather-icon', 'cloud', 'rain', 'strong']);
 
   useEffect(() => {
-    console.log('Произошли изменения');
+    isSun === true && setClasses(['weather-icon', 'sun'])
+    isCloud === true && setClasses(['weather-icon', 'cloud'])
+    isRain === true && setClasses(['weather-icon', 'cloud', 'rain'])
+    isStrongRain === true && setClasses(['weather-icon', 'cloud', 'rain', 'strong'])
+    
   }, [temperature,
-    isSnow,
+    isSun,
+    isCloud,
     isRain,
-    isStrongWind,
+    isStrongRain,
   ]);
 
   return (
     <div className="weather-widget">
-      {/* <div className="weather-search">
-        <input type="text" className="weather-serch_temperature" placeholder="Введите текующую температуру" value={temperature} onChange={event => setTemperature(event.target.value)} />
+      <div className="weather-search">
+        <input type="text" className="weather-serch_temperature" placeholder="Введите текующую температуру" value={temperature} onChange={event => {setTemperature(event.target.value)}} />
         <label>
-          Идёт снег:
-          <input type="checkbox" checked={isSnow} onChange={event => setSnow(event.target.checked)} />
+          Солнечно:
+          <input type="checkbox" checked={isSun} onChange={event => {
+              setCloud(false)
+              setRain(false)
+              setStrongRain(false)
+              setSun(event.target.checked)}
+            } />
         </label>
         <label>
-          Идёт Дождь:
+          Идёт облачно:
+          <input type="checkbox" checked={isCloud} onChange={event => {
+              setSun(false)
+              setCloud(event.target.checked)}
+            } />
+        </label>
+        <label>
+          Идёт дождь:
           <input type="checkbox" checked={isRain} onChange={event => setRain(event.target.checked)} />
         </label>
         <label>
-          Сильный ветер:
-          <input type="checkbox" checked={isStrongWind} onChange={event => setStrongWind(event.target.checked)} />
+          Идёт сильный дождь:
+          <input type="checkbox" checked={isStrongRain} onChange={event => setStrongRain(event.target.checked)} />
         </label>
-      </div> */}
+      </div>
       
       <div className="weather-card">
-        <div className="weather-icon sun"></div>
+        <div className={iconClasses.join(' ')}></div>
 
         <h1 className="temperature">{temperature}°</h1>
         <p className="city">Moscow</p>
